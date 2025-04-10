28a0e74221f9fe3c46ebfa5b4c6d06a374329ff5
chrqweasd123 <chr@qweasd>
2025-04-10 13:59:32 -0300

    package: add retrofit and gson.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
 
     // Tests dependencies
     testImplementation(libs.junit)

716f989dbc0126d94894d252dce13990c4de82fe
chrqweasd123 <chr@qweasd>
2025-04-12 12:15:44 -0300

    package: import activity and lifecycle methods.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,9 @@ android {
 
 dependencies {
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.retrofit)

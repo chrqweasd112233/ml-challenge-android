e9083c372fb7e0d91fdacad1bac0eb68b86515fe
chrqweasd123 <chr@qweasd>
2025-04-13 13:01:24 -0300

    chore: remove retrofit client not used for now

--- app/src/main/java/com/christianalexandre/mlchallengeandroid/data/api/SearchApiService.kt
+++ app/src/main/java/com/christianalexandre/mlchallengeandroid/data/api/SearchApiService.kt
@@ -2,10 +2,7 @@ package com.christianalexandre.mlchallengeandroid.data.api
 
 import com.christianalexandre.mlchallengeandroid.data.model.SearchResponseDTO
 import com.christianalexandre.mlchallengeandroid.data.util.ApiResponse
-import retrofit2.http.GET
-import retrofit2.http.Path
 
 interface SearchApiService {
-    @GET("search?q={query}")
-    suspend fun search(@Path("query") query: String): ApiResponse<SearchResponseDTO>
+    suspend fun search(query: String): ApiResponse<SearchResponseDTO>
 }
